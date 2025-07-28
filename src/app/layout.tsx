38abb7ed8192b951051820a2b0bebdc334f93907
Patrik Beijar Odh <patrik.odh@gmail.com>
2025-07-28 12:50:30 +0200

    08: trpc-setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { TRPCReactProvider } from "@/trpc/client";
+
 
 const inter = Inter({
- 
   subsets: ["latin"],
 });
 
@@ -20,12 +21,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${inter.className}  antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <TRPCReactProvider>
+
+      <html lang="en">
+        <body
+          className={`${inter.className}  antialiased`}
+        >
+          {children}
+        </body>
+      </html>
+    </TRPCReactProvider>
   );
 }
